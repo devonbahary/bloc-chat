3593692d2e8876d58ba97055b70cf5c917a6ed8f
devonbahary <devonbahary@gmail.com>
2017-06-08 19:50:58 -0700

    First progress on 3 Create Chat Rooms, added 'Room.add' method to 'Room.js' and 'createRoom' method to 'HomeCtrl.js'

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,21 @@
 (function() {
   function HomeCtrl($scope, Room) {
+    /*
+     * @desc The $firebaseArray Rooms database from the 'Room' service
+     * @type {Object}
+    */
     $scope.rooms = Room.all;
+
+    /*
+     * @func createRoom
+     * @desc Creates a new room object and adds it to the 'Room.all' database
+    */
+    $scope.createRoom = function() {
+      var newRoom = {};
+      var roomNumber = Room.all.length + 1;
+      newRoom["room" + roomNumber] = "room" + roomNumber;
+      Room.add(newRoom);
+    }
   }
 
   angular
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,10 +1,39 @@
 (function() {
   function Room($firebaseArray) {
+    /*
+     * @desc Our return object, holding all of our factory service's properties
+     * and methods.
+     * type {Object}
+    */
     var Room = {};
+
+    /*
+     * @desc A reference to our Firebase database 'Rooms'
+     * @type {Object}
+    */
     var ref = firebase.database().ref().child("rooms");
+
+    /*
+     * @desc Our 'ref' object from above with new '$firebaseArray' properties
+     * and methods.
+     * @type {Object}
+    */
     var rooms = $firebaseArray(ref);
 
+    /*
+     * @desc A 'Room' property of the above.
+     * @type { Object }
+    */
     Room.all = rooms;
+
+    /*
+     * @func add
+     * @desc Creates a new room object and adds it to the database.
+     * @param {Object}
+    */
+    Room.add = function(room) {
+      Room.all.$add(room);
+    };
 
     return Room;
   }
